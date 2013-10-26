7f7f810dbdcae8ebb6558ff80bbe34e41052a425
Glitchmaster-PE <lucasphillips712@gmail.com>
2013-10-26 19:49:12 -0400

    You should like this ;)

--- Sheep.php
+++ Sheep.php
@@ -1,3 +1,36 @@
 <?php
 
+/*
+__PocketMine Plugin__
+name=Sheep
+version=1.0
+description=Sheep
+author=KnownUnown
+class=Sheep
+apiversion=10
+*/
+
+class Sheep implements Plugin{
+  
+  private $api;
+  
+  public function __construct(ServerAPI $api, $server = false){
+    $api = $this->api;
+  }
+  
+  public function init(){
+    $this->api->console->register("sheep","Sheep",array($this, "Sheep"));
+    $this->api->ban->cmdWhitelist("sheep");
+  }
+  
+  public function Sheep($cmd, $issuer){
+    $issuer->sendChat("Bahhhhhh");
+  }
+  
+  public function __destruct(){
+    
+  }
+  
+}
+
 ?>
